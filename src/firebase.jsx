a5cd9f63dd643bc98c7fb4348812ce84861750e6
Kripa Shankar <kshankar.1109@gmail.com>
2024-08-24 11:40:48 +0530

    initial deploy

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdjH6QMsFk_jgAIWzBGn8pJ4nbaH9VyhY",
+  authDomain: "cyntaxeror.firebaseapp.com",
+  projectId: "cyntaxeror",
+  storageBucket: "cyntaxeror.appspot.com",
+  messagingSenderId: "57587072029",
+  appId: "1:57587072029:web:bdfb0805f05d6d87dcdaf4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
