09642bf82df9aa90c045a06a07e4d549ad362c80
Mrutyunjay <2004lodhi@gmail.com>
2024-08-24 13:53:44 +0530

    bg added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 function Home() {
   return (
-    <div className="bg-scroll bg-cover"
-      style={{ backgroundImage: "url('/bgi.png')" }}>
-      <div className="h-screen w-screen flex flex-row justify-between">
-        <main className="flex-grow  flex justify-center items-center pt-20">
-          <h1>settleIN </h1>
-
+    <div className="bg-[url('/bgi.png')] bg-cover bg-scroll h-screen w-screen">
+      <div className="h-full w-full flex flex-row justify-between">
+        <main className="flex-grow flex justify-center items-center pt-20">
+          <h1 >settleIN</h1>
         </main>
       </div>
     </div>
-
   );
 }
 
